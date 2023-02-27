fe7393cd35a48486542b2f928ef4058c23b6f473
Syahrezaimamw <118792100+Syahrezaimamw@users.noreply.github.com>
2023-02-27 09:38:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let nilai = prompt ("masukan nilai : ", 0);
+let hasil = "";
+
+switch(true){
+    case nilai >= 91 && nilai <= 100:
+        hasil = "A"
+        break;
+
+    case nilai >=81 && nilai <= 90:
+        hasil = "B"
+        break;
+
+    case nilai >=71 && nilai <=80:
+        hasil = "C"
+        break;
+
+    case nilai >=61 && nilai <=70:
+        hasil = "D"
+        break;
+    
+    case nilai >=0 && nilai <=60 :
+        hasil = "F"
+        break;
+    
+    default:
+        document.write("inutan kelebihan");
+
+}
+
+document.write(hasil);
